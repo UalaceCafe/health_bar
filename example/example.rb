8037388ca534df83b2fc4b7b3b6058a932793234
Ualace Henrique Santos Café <ualacehenrique@hotmail.com>
2021-08-28 01:40:35 -0300

    Updated example.rb
    Updated the example to be in accordance with the new syntax/usage of the script

--- example/example.rb
+++ example/example.rb
@@ -6,20 +6,20 @@ set(title: 'Health Bar example', width: 600, height: 400, background: '#205b7d')
 WIDTH = Window.width
 HEIGHT = Window.height
 
-health_bar = HealthBar.new
+health_bar = HealthBar.new(change_color: true)
 
 on :key_held do |event|
   case event.key
   when 'a'
-    health_bar.current_health -= 1
+    health_bar.current -= 1
   when 'd'
-    health_bar.current_health += 1
+    health_bar.current += 1
   when 'w'
-    health_bar.max_health += 10
-    health_bar.current_health = health_bar.max_health
+    health_bar.max += 10
+    health_bar.current = health_bar.max
   when 's'
-    health_bar.max_health -= 10
-    health_bar.current_health = health_bar.max_health
+    health_bar.max -= 10
+    health_bar.current = health_bar.max
   end
 end
 
@@ -28,8 +28,8 @@ update do
 
   health_bar.add
 
-  Text.new("Max health: #{health_bar.max_health}", x: WIDTH - 130, y: 10, size: 15, color: 'yellow')
-  Text.new("Current health: #{health_bar.current_health}", x: WIDTH - 130, y: 30, size: 15, color: 'yellow')
+  Text.new("Max health: #{health_bar.max}", x: WIDTH - 130, y: 10, size: 15, color: 'yellow')
+  Text.new("Current health: #{health_bar.current}", x: WIDTH - 130, y: 30, size: 15, color: 'yellow')
 end
 
 show
