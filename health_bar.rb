5912092ed055e29636b6f7beb7febfd4fb5818ee
Ualace Henrique Santos Café <ualacehenrique@hotmail.com>
2022-03-02 19:18:50 -0300

    Remove dependency and improve the script

--- health_bar.rb
+++ health_bar.rb
@@ -1,9 +1,10 @@
 require 'ruby2d'
-require 'math2d'
 
 class HealthBar < Ruby2D::Entity
   attr_reader :max
-  attr_accessor :current
+  attr_accessor :x, :y, :width, :height, :color1, :color2, :background, :change_color, :current
+
+  FRAME_SIZE = 2
 
   def initialize(opts = {})
     @x = opts[:x] || 15
@@ -23,7 +24,7 @@ class HealthBar < Ruby2D::Entity
 
   def max=(value)
     raise ArgumentError, 'max health cannot be less or equal to zero' if value <= 0
-    
+
     @max = value
   end
 
@@ -33,31 +34,51 @@ class HealthBar < Ruby2D::Entity
     @current = 0 if @current.negative?
     @current = @max if @current > @max
 
-    frame_size = 2
     Rectangle.new(x: @x, y: @y, width: @width, height: @height, color: 'black')
-    Rectangle.new(x: @x + frame_size, y: @y + frame_size, width: @width - 2 * frame_size, height: @height - 2 * frame_size, color: @background)
+    Rectangle.new(x: @x + FRAME_SIZE, y: @y + FRAME_SIZE, width: @width - 2 * FRAME_SIZE, height: @height - 2 * FRAME_SIZE, color: @background)
 
-    bar_width = Utils2D.map(@current, 0, @max, 0, @width - 2 * frame_size)
+    bar_width = map(@current, 0, @max, 0, @width - 2 * FRAME_SIZE)
     color = if @change_color
               amount = @current / @max.to_f
 
-              lerp_hex(@color1.gsub('#', '').to_i(16), @color2.gsub('#', '').to_i(16), amount)
+              if @color1.is_a?(String) && @color2.is_a?(String)
+                lerp_hex(@color1.gsub('#', '').to_i(16), @color2.gsub('#', '').to_i(16), amount)
+              elsif @color1.is_a?(Array) && @color2.is_a?(Array)
+                r = lerp(@color2[0], @color1[0], amount)
+                g = lerp(@color2[1], @color1[1], amount)
+                b = lerp(@color2[2], @color1[2], amount)
+                a = lerp(@color2[3], @color1[3], amount)
+                [r, g, b, a]
+              else
+                raise ArgumentError, 'both `color1` and `color2` must be of the same type: either a Hex String or a Normalized RGBA Array'
+              end
             else
               @color1
             end
-    Rectangle.new(x: @x + frame_size, y: @y + frame_size, width: bar_width, height: @height - 2 * frame_size, color: color)
+    Rectangle.new(x: @x + FRAME_SIZE, y: @y + FRAME_SIZE, width: bar_width, height: @height - 2 * FRAME_SIZE, color: color)
   end
 
   private
+
+  def map(value, a1, a2, b1, b2)
+    raise ArgumentError, 'division by 0 when mapping two ranges - a1 cannot be equal to a2' if a2 == a1
+
+    slope = 1.0 * (b2 - b1) / (a2 - a1)
+    b1 + slope * (value - a1)
+  end
+
+  def lerp(a, b, amt)
+    (b - a) * (3.0 - amt * 2.0) * amt * amt + a
+  end
 
   def lerp_hex(a, b, amt)
     a_rgb = [a >> 16, a >> 8 & 0xFF, a & 0xFF]
     b_rgb = [b >> 16, b >> 8 & 0xFF, b & 0xFF]
 
     value = [
-      Utils2D.lerp(b_rgb[0], a_rgb[0], amt).to_i,
-      Utils2D.lerp(b_rgb[1], a_rgb[1], amt).to_i,
-      Utils2D.lerp(b_rgb[2], a_rgb[2], amt).to_i
+      lerp(b_rgb[0], a_rgb[0], amt).to_i,
+      lerp(b_rgb[1], a_rgb[1], amt).to_i,
+      lerp(b_rgb[2], a_rgb[2], amt).to_i
     ]
 
     hex = ((1 << 24) + (value[0] << 16) + (value[1] << 8) + value[2] | 0).to_s(16)
